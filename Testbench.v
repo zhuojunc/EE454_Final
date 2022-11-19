04cd962ea35cf8d20e4830c7ffa650ccfeee3628
Zhuojun Chen <zhuojunc@usc.edu>
2022-11-18 19:43:38 -0800

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -39,7 +39,9 @@ module axiProtocol_tb;
 	wire [11:0] AWOUT;
 	wire [7:0] WDATA;
 	wire [4:0] BOUT;
-
+	
+	integer i;
+	integer j;
 
 Master DUT(.rst(rst), .clk(clk), .en(en), .en_(en_), .tb_R(tb_R), .IN(SOUT), .tb_W(tb_W), .INDATA(INDATA), 
 .ARREADY(ARREADY), .RVALID(RVALID), .RLAST(RLAST), .AWREADY(AWREADY), .WREADY(WREADY), .BVALID(BVALID), .BRESP(BRESP),
@@ -64,7 +66,7 @@ begin
     tb_R = 0;
     tb_W = 0;
 
-    // test case 1 (write 3 data)
+    // test case 1 (write 8 data)
     rst = 1;
     #20
     rst = 0;
@@ -81,19 +83,14 @@ begin
         INDATA[55:48] = 8'b00000011;
 	INDATA[63:56] = 8'b00000100;
 
-	// INDATA[71:64] = 8'b00000001;
-    //     INDATA[79:72] = 8'b00000010;
-    //     INDATA[87:80] = 8'b00000011;
-	// INDATA[95:88] = 8'b00000100;
-
-	// INDATA[103:96] = 8'b00000001;
-    //     INDATA[111:104] = 8'b00000010;
-    //     INDATA[119:112] = 8'b00000011;
-	// INDATA[127:120] = 8'b00000100;
-
         tb_W[15:8] = 8'b00000001;
         tb_W[7:4] = 4'b0111;
         tb_W[3:0] = 4'b0001;
+		
+		$display("Starting WRITE %d", tb_W[3:0]);
+	    for (i = 0; i <= tb_W[7:4]; i = i + 1) begin
+			$display("Expected WRITE data %d to ADDR %d", INDATA[i * 8 +: 8], tb_W[15:8] + i);
+		end		
 	
         en_ = 1;
         #20 
@@ -101,7 +98,7 @@ begin
 
 
 
-    // test case 2 (read 3 data)
+    // test case 1 (read 8 data)
     #4000
     rst = 1;
     #20
@@ -112,11 +109,133 @@ begin
         tb_R[15:8] = 8'b00000001;
         tb_R[7:4] = 4'b0111;
         tb_R[3:0] = 4'b0001;
+		
+		$display("\nStarting READ %d", tb_R[3:0]);
+	    for (j = 0; j <= tb_R[7:4]; j = j + 1) begin
+			$display("Expected READ data %d starting at ADDR %d", INDATA[j * 8 +: 8], tb_R[15:8]);
+		end
+		
         en = 1;
         #20 
         en = 0;
+	
+	#4000
+    INDATA = 0;
+    tb_R = 0;
+    tb_W = 0;
+
+    // test case 2 (write 16 data)
+    rst = 1;
+    #20
+    rst = 0;
+
+        // BURST WRITE
+        #10
+        INDATA[7:0] = 8'b00000001;
+        INDATA[15:8] = 8'b00000010;
+        INDATA[23:16] = 8'b00000011;
+	INDATA[31:24] = 8'b00000100;
+
+	INDATA[39:32] = 8'b00000001;
+        INDATA[47:40] = 8'b00000010;
+        INDATA[55:48] = 8'b00000011;
+	INDATA[63:56] = 8'b00000100;
+
+	INDATA[71:64] = 8'b00000001;
+        INDATA[79:72] = 8'b00000010;
+        INDATA[87:80] = 8'b00000011;
+	INDATA[95:88] = 8'b00000100;
+
+	INDATA[103:96] = 8'b00000001;
+        INDATA[111:104] = 8'b00000010;
+        INDATA[119:112] = 8'b00000011;
+	INDATA[127:120] = 8'b00000100;
+
+        tb_W[15:8] = 8'b00000001;
+        tb_W[7:4] = 4'b1111;
+        tb_W[3:0] = 4'b0010;
+		
+		$display("\nStarting WRITE %d", tb_W[3:0]);
+	    for (i = 0; i <= tb_W[7:4]; i = i + 1) begin
+			$display("Expected WRITE data %d to ADDR %d", INDATA[i * 8 +: 8], tb_W[15:8] + i);
+		end		
+	
+        en_ = 1;
+        #20 
+        en_ = 0;
 
 
+
+    // test case 2 (read 16 data)
+    #8000
+    rst = 1;
+    #20
+    rst = 0;
+
+        // BURST READ
+        #10
+        tb_R[15:8] = 8'b00000001;
+        tb_R[7:4] = 4'b1111;
+        tb_R[3:0] = 4'b0010;
+		
+		$display("\nStarting READ %d", tb_R[3:0]);
+	    for (j = 0; j <= tb_R[7:4]; j = j + 1) begin
+			$display("Expected READ data %d starting at ADDR %d", INDATA[j * 8 +: 8], tb_R[15:8]);
+		end
+		
+        en = 1;
+        #20 
+        en = 0;	
+
+	#8000
+	INDATA = 0;
+    tb_R = 0;
+    tb_W = 0;
+
+    // test case 3 (write 1 data)
+    rst = 1;
+    #20
+    rst = 0;
+
+        // BURST WRITE
+        #10
+        INDATA[7:0] = 8'b00000001;
+
+        tb_W[15:8] = 8'b00000001;
+        tb_W[7:4] = 4'b0000;
+        tb_W[3:0] = 4'b0011;
+		
+		$display("\nStarting WRITE %d", tb_W[3:0]);
+	    for (i = 0; i <= tb_W[7:4]; i = i + 1) begin
+			$display("Expected WRITE data %d to ADDR %d", INDATA[i * 8 +: 8], tb_W[15:8] + i);
+		end		
+	
+        en_ = 1;
+        #20 
+        en_ = 0;
+
+
+
+    // test case 3 (read 1 data)
+    #500
+    rst = 1;
+    #20
+    rst = 0;
+
+        // BURST READ
+        #10
+        tb_R[15:8] = 8'b00000001;
+        tb_R[7:4] = 4'b0000;
+        tb_R[3:0] = 4'b0011;
+		
+		$display("\nStarting READ %d", tb_R[3:0]);
+	    for (j = 0; j <= tb_R[7:4]; j = j + 1) begin
+			$display("Expected READ data %d starting at ADDR %d", INDATA[j * 8 +: 8], tb_R[15:8]);
+		end
+		
+        en = 1;
+        #20 
+        en = 0;
 end
 
 endmodule
